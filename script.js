65b96f2ee2a26382f5aacabbe073aa6ecf2e07ee
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-09-18 22:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	let newRow=table.insertRow(0);
 
 	let cell1=newRow.insertCell(0);
-	cell1.innerHTML="new Cell1";
+	cell1.innerHTML="New Cell1";
 	let cell2=newRow.insertCell(1);
-	cell2.innerHTML="new Cell2";
+	cell2.innerHTML="New Cell2";
   
 }
